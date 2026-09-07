9434693339adaf1e4a215ad6a36ef59f78f587f1
Alex Bui <abui0012@student.monash.edu>
2026-09-08 06:37:24 +1000

    Add HTML boilerplate and auth plug
    - Added simple boilerplate for HTML content
    - Added PDO and auth plug to check authentication

--- animals/list.php
+++ animals/list.php
@@ -1,1 +1,25 @@
 <?php
+session_start();
+require_once __DIR__ . '/../auth/authentication.php';
+
+/** @var PDO $pdo */
+require_once __DIR__ . '/../connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animals - SafePaws Admin</title>
+    <!-- Link your external CSS file here -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- Your visible website content goes here -->
+
+<!-- Link your external JavaScript file here -->
+<script src="script.js"></script>
+</body>
+</html>
