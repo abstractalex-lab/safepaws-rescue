32f9c31a4be74f6f46cee941e201593760e8a022
Alex Bui <abui0012@student.monash.edu>
2026-09-08 12:19:12 +1000

    Create public animals.php file with basic HTML boilerplate

---
+++ public/animals.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animals for Adoption - SafePaws Rescue</title>
+</head>
+<body>
+    <h1>Animals for Adoption</h1>
+</body>
+</html>
